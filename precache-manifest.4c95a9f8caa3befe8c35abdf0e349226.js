cbf51a272f71b1786a4e4330baf34f79bb84ed03
hsinyuh2 <hsinyuh2@illinois.edu>
2019-03-25 16:46:31 -0500

    Updates

---
+++ precache-manifest.4c95a9f8caa3befe8c35abdf0e349226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4881dd9cb4f5a66755b",
+    "url": "/overcook/static/js/runtime~main.fd020156.js"
+  },
+  {
+    "revision": "dff1c606b0e6c317c657",
+    "url": "/overcook/static/js/main.7009286c.chunk.js"
+  },
+  {
+    "revision": "861892db95cb65ba8ee6",
+    "url": "/overcook/static/js/2.73a4873d.chunk.js"
+  },
+  {
+    "revision": "dff1c606b0e6c317c657",
+    "url": "/overcook/static/css/main.aaf809cd.chunk.css"
+  },
+  {
+    "revision": "91282e36df771e0a580083fbafacfcbc",
+    "url": "/overcook/index.html"
+  }
+];
